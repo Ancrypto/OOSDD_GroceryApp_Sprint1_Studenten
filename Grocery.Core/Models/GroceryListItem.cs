97ec781baf55211a71959539e7e9720bb56b9e5d
Marnix <s1201490@student.windesheim.nl>
2025-09-11 19:33:57 +0200

    Properties correct toegevoegd

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,9 +2,37 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
-        public int Amount { get; set; }
+        private int groceryListId;
+		public int GroceryListId
+        {
+            get
+            {
+                return groceryListId;
+            }
+            set
+            {
+				groceryListId = value;
+
+			}
+        }
+
+
+		private int productId;     //To do: change it to a property
+		public int ProductId
+		{
+			get
+			{
+				return productId;
+			}
+			set
+			{
+				productId = value;
+
+			}
+		}
+
+
+		public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
             //To do:assign values to the properties
